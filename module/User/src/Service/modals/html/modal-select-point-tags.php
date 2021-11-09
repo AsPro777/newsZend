9271d492272f6970eb9442d1e7ce3c747e487f74
AsPro777 <92028919+AsPro777@users.noreply.github.com>
2021-11-09 15:51:04 +0300

    commit

--- module/User/src/Service/modals/html/modal-select-point-tags.php
+++
@@ -1,8 +0,0 @@
-<?php
-return <<<S
-        <div class="panel">
-            <table class="table table-bordered table-xxs">
-                {$table_rows}
-            </table>
-        </div>
-S;
--- module/User/src/Service/modals/html/modal-time.php
+++
@@ -1,8 +0,0 @@
-<?php
-return <<<S
-    <div class="row">
-        <div class="col-md-12" id="field-cargo">
-                <input name="date_start" class="form-control" placeholder="" value="" type="text" style="text-align:center;font-weight:bolder;font-size:20px">
-        </div>
-    </div>
-S;
